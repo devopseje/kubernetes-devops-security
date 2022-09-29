ea2c7faac4c729897684ba561ba808abe9231b1b
Guillaume Ejangue <97439607+devopseje@users.noreply.github.com>
2022-09-29 22:31:14 +0200

    change springboot version

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,7 +1,9 @@
 package com.devsecops;
 
 
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
